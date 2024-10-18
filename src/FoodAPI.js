3fe71a787beaa11c7e022b4cfa19617bd6e90cfe
T REDDI PRAKASH <reddiprakasht1122@gmail.com>
2024-10-18 12:15:20 +0530

    i changed foodCard component to grid by removing the fixed width and heights and make it responsive and also make FoodApi component responsive of image with bootstrap img-fluid and changed input with some padding and margin

--- src/FoodAPI.js
+++ src/FoodAPI.js
@@ -3,34 +3,37 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import FoodCard from './FoodCard';
 import pic from "./images/food-banner.png"
 export default function FoodAPI() {
-    const [search,setSearch]=useState("");
-  const [data,setData]=useState([]);
-  
-  const YOUR_APP_ID ="d1adb527";
-  const YOUR_APP_KEY="fd8b9693f27978e90b3a63132addbeff";
-  const submitHandler =(e)=>{
-   e.preventDefault();
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  const YOUR_APP_ID = "d1adb527";
+  const YOUR_APP_KEY = "fd8b9693f27978e90b3a63132addbeff";
+  const submitHandler = (e) => {
+    e.preventDefault();
     // console.log(search);
     fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=12&calories=591-722&health=alcohol-free`)
-    .then(response=>response.json())
-    .then(data=>setData(data.hits)
-    )
+      .then(response => response.json())
+      .then(data => setData(data.hits)
+      )
   }
   return (
-    <div>
-       <img className='food-pic' src={pic}></img>
-      <center>
-        <h4 className='head'> Look for Your Food Receipe </h4>
-        <form onSubmit={submitHandler}>
-          <input type='text' className='mt-3' value={search} onChange={(e)=>setSearch(e.target.value)}></input>
-          <br></br>
-          <input type='submit' className='btn btn-primary mt-3' value="Search"></input>
-        </form>
+    <div className='row'>
+      <div className='col-12'>
+        <div className='d-flex justify-content-center'>
+          <img className='food-pic img-fluid rounded-5 mt-2' src={pic} alt="Food Banner" />
+        </div>
+        <center>
+          <h4 className='head'> Look for Your Food Receipe </h4>
+          <form onSubmit={submitHandler}>
+            <input type='text' className='mt-3 form-control p-5' placeholder='Search Food Recipie' value={search} onChange={(e) => setSearch(e.target.value)}></input>
+            <br></br>
+            <input type='submit' className='btn btn-success px-5 mt-3 rounded-5' value="Search"></input>
+          </form>
 
-        {data.length>1 ?<FoodCard  data={data}/>:null}   
-       
-      </center>
+          {data.length > 1 ? <FoodCard data={data} /> : null}
 
+        </center>
+      </div>
     </div>
   )
 }
--- src/FoodCard.js
+++ src/FoodCard.js
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-export default function FoodCard({data}) {
+export default function FoodCard({ data }) {
   return (
-    <div>
-        <div  className='row'>
-            {data.map(data=>
-      <div className="card food" style={{"width": "300px","height":"400px","margin-left":"20px","margin-top":"30px"}}>
-  <img className="card-img-top box-img" src={data.recipe.image}  alt="Card image cap" style={{"width": "100%","margin-top":"10px"}}/>
-
-  <div className="card-body">
-    <center>
-    <h5 className="card-title">{data.recipe.label}</h5>
-    <p className="card-text">Total Amount of Calories :{Math.round(data.recipe.calories)}</p>
-    <a href="#" className="btn btn-primary">Buy</a>
-    
-    </center>
-  </div>
-</div>
- )} 
-     </div>
-    </div>
-              
-  )
+   
+      <div className="row mt-5 ">
+        {data.map((item, index) => (
+          <div key={index} className="col-12 col-sm-12 col-md-4 col-lg-6 col-xl-4 col-xxl-4 mb-2">
+            <div className="card food">
+              <img
+                className="card-img-top box-img"
+                src={item.recipe.image}
+                alt="Card image cap"
+              />
+              <div className="card-body">
+                <center>
+                  <h5 className="card-title">{item.recipe.label}</h5>
+                  <p className="card-text">
+                    Total Amount of Calories: {Math.round(item.recipe.calories)}
+                  </p>
+                  <a href="#" className="btn btn-primary">Buy</a>
+                </center>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+  );
 }
-
-
-
